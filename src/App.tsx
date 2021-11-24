5db229e82ac437870c62adde5318cd3a2fa06e5a
user_nitro_5 <timmy.lepel@gmail.com>
2021-11-24 06:34:10 +0300

    basic style searchBox

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './App.css'
+import {Header} from './pages/Header'
+import libraryImg from '../src/assets/img/bookImg.jpg'
+
+const appStyle = {
+    backgroundImage: `url(${libraryImg})`,
+    height: '100vh',
+    opacity: '0.9',
+    backgroundSize:'cover',
+    backgroundAttachment:'fixed'
+}
 
 function App() {
     return (
-        <div className="App">
-            google books
+        <div style={appStyle}>
+            <Header/>
         </div>
     )
 }
